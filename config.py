2adfbb4e97ac43fcd6d407272c9b5bb321fc283b
l49 <0249wl@gmail.com>
2026-02-02 19:44:18 +0800

    优化

--- config.py
+++ config.py
@@ -5,6 +5,9 @@ from dotenv import load_dotenv
 # 加载 .env 文件（如果存在）
 load_dotenv()
 
+# 环境变量配置
+os.environ.setdefault("HF_ENDPOINT", "https://hf-mirror.com")  # 使用国内镜像加速模型下载
+
 # 请在此处替换为你的真实API密钥
 API_KEY = os.environ.get("DASHSCOPE_API_KEY")
 # 或其他你使用的模型API密钥
